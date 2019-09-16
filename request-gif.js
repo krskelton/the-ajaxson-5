50bc8d45680722de130c05c5d5950523f40801a2
Kristin Skelton <kskelton82@gmail.com>
2019-09-16 10:30:42 -0600

    Final AJAXson 5

--- request-gif.js
+++ request-gif.js
@@ -7,38 +7,48 @@ var ajaxson5 = new Vue({
             errorMessage: null,
             loading: false,
             imgSrc: null,
+            captcha: null
         };
     },
     methods: {
         fetchGif: function() {
-            // get the user's input text from the DOM
-            var searchQuery = ""; // TODO should be e.g. "dance"
+            if(this.captcha != 5){
+                this.errorMessage = 'No GIFs for you';
+                this.imgSrc = null;
+            } else{
+                // get the user's input text from the DOM
+                var searchQuery = this.tagValue; // TODO should be e.g. "dance"
 
-            // configure a few parameters to attach to our request
-            var api_key = "dc6zaTOxFJmzC";
-            var tag = ""; // TODO should be e.g. "jackson 5 dance"
+                // configure a few parameters to attach to our request
+                var api_key = "dc6zaTOxFJmzC";
+                var tag = "jackson+5+" + searchQuery; // TODO should be e.g. "jackson 5 dance"
+                // console.log(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`);
 
-            // TODO what do we want this URL to be?
-            fetch(`http://example.com?api_key=${api_key}&tag=${tag}`)
-                .then(response => response.ok ? response.json() : Promise.reject(response))
-                .then(results => {
-                    // if the response comes back successfully, the code in here will execute.
+                // TODO what do we want this URL to be?
+                fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
+                    .then(response => response.ok ? response.json() : Promise.reject(response))
+                    .then(results => {
+                        // if the response comes back successfully, the code in here will execute.
 
-                    console.log("we received a response!");
-                    console.log(results);
-                    // TODO
-                    // 1. set the imgSrc value in our data to the GIF's image_url inside results
-                    // 2. clear the error message and loading state (since our request just succeede)
-                })
-                .catch(err => {
-                    // if something went wrong, the code in here will execute instead of the success function
+                        console.log("we received a response!");
+                        console.log(results);
 
-                    this.loading = false;
-                    this.errorMessage = 'Sorry, could not load GIF. Try again!';
-                });
-            // TODO We've just made a request, so this is a good time to
-            // set "loading = true"
+                        this.imgSrc = results.data.image_url;
+                        // console.log(results.data.image_url)
 
+                        this.errorMessage = null;
+                        this.loading = null;
+                    })
+                    .catch(err => {
+                        // if something went wrong, the code in here will execute instead of the success function
+
+                        this.loading = false;
+                        this.errorMessage = 'Sorry, could not load GIF. Try again!';
+                    });
+                    this.loading = true;
+            }
+
+           
         },
     },
 });
